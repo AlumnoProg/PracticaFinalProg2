8d16b6bfa8123ac088a60866a1152326209b8f62
Programa CionDos <progciondos@gmail.com>
2022-12-12 19:30:48 -0300

    Ejercicios de colas y arboles del final

---
+++ Ejercicios Arboles y Colas Finales/Ej.dpr
@@ -0,0 +1,14 @@
+program Ej;
+
+uses
+  Vcl.Forms,
+  Ejercicio in 'Ejercicio.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
